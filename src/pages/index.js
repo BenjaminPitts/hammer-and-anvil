5722273f469696544171bbd4ec05e45db1fe39b5
Ben Pitts <benjaminjackpittsAgmail.com>
2022-07-08 09:53:08 -0600

    adding m-time img to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,7 +69,7 @@ const IndexPage = () => (
       width={400}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
-      alt="Benjamin Pitts"
+      alt="Gianni DiGiacomo"
       style={{ margin: `1rem` }}
     />
     </strong>
@@ -77,6 +77,14 @@ const IndexPage = () => (
     <div class='text-block'>
     <h3><i>"Hello, my name is Gianni DiGiacomo and I am a musician from Denver, CO and advocate to protect hearing in my community. I own and operate Hammer & Anvil hearing, a company that provides remote hearing, testing, and hearing aid services. We provide custom musician earplugs, in-ear monitors, hearing aids, hearing aid maintainence & repair, educational resources & consultation, assisted listening devices. I've worked in Audiology for over ten years and have the experience and knowledge to provide an excellent patient experience. If you need help determining weather you need hearing aids, or just need assistance with maintaining or repairing your existing hearing aids, I'd love to meet you and help you out!"</i></h3>
     </div>
+    <StaticImage
+      src="../images/m-time.jpeg"
+      width={400}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="Gianni DiGiacomo"
+      style={{ margin: `1rem` }}
+    />
 
   </Layout>
 )
